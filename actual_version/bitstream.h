2b4b7cc6131deba1fb0b6cf9deb0948006077cc6
Andrew Betts <a.c.betts@btinternet.com>
2015-10-30 17:32:24 +0000

    Moved functions for inserting and getting bits in a block from hamming.c to bitstream.c/h. This is useful for future work that will use these functions and different encoding methods.

---
+++ actual_version/bitstream.h
@@ -0,0 +1,8 @@
+
+//allows bits to be inserted or got from a position in a block of memory.
+//functions that take int* position will modify position by the number
+//of bits stored or retrieved
+
+void insert_bits_at_position(uint8_t* data, uint8_t bits, int num_bits, int* position);
+
+uint8_t get_bits_from_position(const uint8_t* data, int num_bits, int* position);
--- actual_version/hamming.c
+++ actual_version/hamming.c
@@ -1,6 +1,7 @@
 #include "hamming.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "bitstream.h"
 
 //encodes 4 bits to 7 using Hamming(4,7) ECC
 uint8_t hamming47_lookup(uint8_t input)
@@ -34,50 +35,6 @@ uint8_t check_hamming47_lookup(uint8_t input)
 		0x1, 0x8, 0x5, 0xf, 0x3, 0xf, 0xf, 0xf};
 
 	return lookup[input];
-}
-
-//data is data stream to insert into, bits is literal bits, num_bits is number of bits (<=8)
-//position is position in data stream in bits
-void insert_bits_at_position(uint8_t* data, uint8_t bits, int num_bits, int* position)
-{
-  data += *position / 8;
-  int bit_position = *position % 8;
-  uint8_t bits_copy = bits;  //bits may be needed unaltered later when spanning bytes
-  bits_copy <<= 8 - num_bits;
-  bits_copy >>= bit_position;
-  *data |= bits_copy;  //leave rest of byte unaltered
-  if(bit_position > 8 - num_bits)
-  {
-    //need to put lower part of bits in next byte
-    int bits_left = bit_position - (8 - num_bits);
-    bits <<= 8 - bits_left;
-    //no need to shift left, as bits will start at beginning of byte (MSB)
-    *(data+1) |= bits;  //leave rest of byte unaltered
-  }
-  *position += num_bits;
-}
-
-uint8_t get_bits_from_position(const uint8_t* data, int num_bits, int* position)
-{
-  int bits_left_in_byte = 8 - (*position % 8);
-  if(bits_left_in_byte > num_bits)
-    bits_left_in_byte = num_bits;
-  int bits_in_next_byte = num_bits - bits_left_in_byte;
-
-  uint8_t ret;
-  if(*position % 8 == 0)
-    ret = data[*position / 8] >> 1;
-  else
-    ret = (data[*position / 8] & ((1 << bits_left_in_byte) - 1)) << bits_in_next_byte;
-
-  if(bits_in_next_byte > 0)
-  {
-    //extract more from next byte
-    ret |= data[*position / 8 + 1] >> (8 - bits_in_next_byte);
-  }
-  *position += num_bits;
-
-  return ret;
 }
 
 raw_data encode_block(raw_data rd)
