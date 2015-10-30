85cea16656e6e03f62b284ce9436f49bf17f26c6
Andrew Betts <a.c.betts@btinternet.com>
2015-10-30 17:36:35 +0000

    Added define guards to bitstream.h

--- actual_version/bitstream.h
+++ actual_version/bitstream.h
@@ -3,6 +3,11 @@
 //functions that take int* position will modify position by the number
 //of bits stored or retrieved
 
+#ifndef BITSTREAM_H
+#define BITSTREAM_H
+
 void insert_bits_at_position(uint8_t* data, uint8_t bits, int num_bits, int* position);
 
 uint8_t get_bits_from_position(const uint8_t* data, int num_bits, int* position);
+
+#endif  //BITSTREAM_H
