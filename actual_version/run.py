aba5c2a752001374ce632691b1f73cc67f2b5310
Andrew Betts <a.c.betts@btinternet.com>
2015-12-03 16:48:43 +0000

    Changed run.py so it can run on any number of cores simultaneously. This should be useful for running on lyceum2.

--- actual_version/run.py
+++ actual_version/run.py
@@ -2,7 +2,8 @@ import subprocess
 import sys
 import time
 
-num_tests = "100"  #string to pass to api_test.exe
+num_tests = "20"  #string to pass to api_test.exe
+num_cores = 4
 
 #arguments to run.py is csv file for which SNRs to look up BERs to run
 
@@ -26,39 +27,27 @@ if __name__ == "__main__":
     num_waiting = 0
 
     #run the processes 4 at a time
-    for i in range(0, len(csv_contents)-4, 4):
-        BER0 = csv_contents[i][1]
-        BER1 = csv_contents[i+1][1]
-        BER2 = csv_contents[i+2][1]
-        BER3 = csv_contents[i+3][1]
-        BERs += [BER0, BER1, BER2, BER3]
-        SNRs += [csv_contents[i][0], csv_contents[i+1][0], csv_contents[i+2][0], csv_contents[i+3][0]]
+    for i in range(0, len(csv_contents)-num_cores, num_cores):
+        BER = []
+        SNR = []
+        for j in range(num_cores):
+            BER.append(csv_contents[i+j][1])
+            SNR.append(csv_contents[i+j][0])
+        BERs += BER
+        SNRs += SNR
 
-        processes.append(subprocess.Popen(["api_test.exe", num_tests, BER0], stdout=subprocess.PIPE))
-        processes.append(subprocess.Popen(["api_test.exe", num_tests, BER1], stdout=subprocess.PIPE))
-        processes.append(subprocess.Popen(["api_test.exe", num_tests, BER2], stdout=subprocess.PIPE))
-        processes.append(subprocess.Popen(["api_test.exe", num_tests, BER3], stdout=subprocess.PIPE))
-        try:
-            outputs.append(float(processes[-4].communicate()[0]))
-        except ValueError:
-            outputs.append(-1.0)
-        try:
-            outputs.append(float(processes[-3].communicate()[0]))
-        except ValueError:
-            outputs.append(-1.0)
-        try:
-            outputs.append(float(processes[-2].communicate()[0]))
-        except ValueError:
-            outputs.append(-1.0)
-        try:
-            outputs.append(float(processes[-1].communicate()[0]))
-        except ValueError:
-            outputs.append(-1.0)
+        for j in range(num_cores):
+            processes.append(subprocess.Popen(["api_test.exe", num_tests, BER[j]], stdout=subprocess.PIPE))
+        for j in range(num_cores):
+            try:
+                outputs.append(float(processes[-num_cores+j].communicate()[0]))
+            except ValueError:
+                outputs.append(-1.0)
 
         print("%i / %i done     " % (i, len(csv_contents)), end="\r")
 
     #run any remaining processes
-    for i in range((len(csv_contents)-4) // 4 * 4, len(csv_contents)):
+    for i in range((len(csv_contents)-num_cores) // num_cores * num_cores, len(csv_contents)):
         BER = csv_contents[i][1]
         BERs.append(BER)
         SNRs.append(csv_contents[i][0])
